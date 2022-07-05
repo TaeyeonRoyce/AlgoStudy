2b63ad59ffd1a2924d9f6136d5d2db1795a8a6dd
TaeyeonRoyce <skdlxmtpqms@gmail.com>
2022-07-05 13:36:15 +0900

    Algo Study Week_02 : Programmers FineSquare

--- src/algorithms/week02/Programmers_FineSquare.java
+++ src/algorithms/week02/Programmers_FineSquare.java
@@ -1,2 +1,33 @@
-package algorithms.week02;public class Programmers_FineSquare {
+package algorithms.week02;
+
+public class Programmers_FineSquare {
+	public static void main(String[] args) {
+		System.out.println(solution(8, 12));
+	}
+
+	public static long solution(int w, int h) {
+		//to Long Type
+		// w <= 100,000,000 , h <= 100,000,000
+		long longW = (long) w;
+		long longH = (long) h;
+		long wholeSquareCnt = longW * longH;
+
+		//"w" should always be bigger than "h"
+		if (longW < longH) {
+			long tmp = longW;
+			longW = longH;
+			longH = tmp;
+		}
+
+		long gcd = findGCD(longW, longH);
+		return wholeSquareCnt - (longW + longH - gcd);
+	}
+
+	private static long findGCD(long a, long b) {
+		if (b == 0) {
+			return a;
+		}
+
+		return findGCD(b, a % b);
+	}
 }
