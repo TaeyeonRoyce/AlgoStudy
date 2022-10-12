a268986ded8e17d20544abc02565460dd7a98cdb
MinSungKim <timkim01@naver.com>
2022-10-12 21:12:59 +0900

    Algo Study Week_17 : Baekjoon_1085

---
+++ src/minsung/week17/Baekjoon_1085.java
@@ -0,0 +1,21 @@
+package minsung.week17;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Baekjoon_1085 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+
+        int result = Math.min(Math.min(w - x, x), Math.min(h - y, y));
+        System.out.println(result);
+    }
+}
