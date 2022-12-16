88f87e41cc4de8f845935bafd1b64a80050934e4
MinSungKim <timkim01@naver.com>
2022-12-17 02:31:35 +0900

    Algo Study Week_26 : Baekjoon_10989

--- src/minsung/week26/Baekjoon_10989.java
+++ src/minsung/week26/Baekjoon_10989.java
@@ -1,4 +1,4 @@
-package minsung.week24;
+package minsung.week26;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -6,20 +6,25 @@ import java.io.InputStreamReader;
 
 public class Baekjoon_10989 {
     public static void main(String[] args) throws IOException {
+        int[] cnt = new int[10001];
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[10001];
 
         for (int i = 0; i < n; i++) {
-            int idx = Integer.parseInt(br.readLine());
-            arr[idx]++;
+            cnt[Integer.parseInt(br.readLine())]++;
         }
 
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 1; i < 10001; i++) {
-            for (int j = 0; j < arr[i]; j++) {
-                System.out.println(i);
+            while(cnt[i] > 0){
+                sb.append(i).append('\n');
+                cnt[i]--;
             }
         }
+
+        System.out.println(sb.toString().strip());
     }
 }
