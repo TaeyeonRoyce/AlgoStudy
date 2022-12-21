0d08126534727e77362d0bd01d08e7d151c77881
MinSungKim <timkim01@naver.com>
2022-12-21 18:43:31 +0900

    Algo Study Week_26 : Baekjoon_11399

---
+++ src/minsung/week26/Baekjoon_11399.java
@@ -0,0 +1,33 @@
+package minsung.week26;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Baekjoon_11399 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                sum += arr[j];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
